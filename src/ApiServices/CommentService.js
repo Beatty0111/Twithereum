d25546e7719ae4adb33947d24a065ef4b70b2996
Ryan Young <ryanyoung1@hotmail.com>
2019-03-02 15:57:02 -0500

    Added Mockup to Commenting Service

--- src/ApiServices/CommentService.js
+++ src/ApiServices/CommentService.js
@@ -1,5 +1,18 @@
-const CommentService = {
-	getComment: (address) => {
+if (Config.mockup) {
+	const CommentService = {
+		getComment: (address) => {
 
+		}
+	}
+} else {
+	const CommentService = {
+		getComment: (address) => {
+			let data = {
+				upvoteNum: 3,
+				comment: "D OMEGALUL C",
+				authorUser: "0x0000000000000000000000000000000000000000",
+				timeStamp: "12-22-2018 00:12:54"
+			};
+		}
 	}
 }
