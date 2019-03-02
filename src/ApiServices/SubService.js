edb8d7320478ffc5a037a15899c4cd747d2af142
Ryan Young <ryanyoung1@hotmail.com>
2019-03-02 15:22:23 -0500

    Added mockup for SubService

--- src/ApiServices/SubService.js
+++ src/ApiServices/SubService.js
@@ -1,9 +1,25 @@
-const SubService = {
-	getPosts: (subAddress) => {
+if (Config.mockup == false ) {
+	const SubService = {
+		getPosts: (address) => {
 
-	},
+		},
 
-	exists: (subName) => {
+		exists: (subName) => {
 
+		}
+	}
+} else {
+	const SubService = {
+		getPosts: (address) => {
+			let data = [
+				"0x747f9da8dffbe1310ec71794f22c37eaee791810f9676d26c0251a465a50f0a1",
+				"0x7ff2e589ca340a50523a827f2a0fc4d5fa3155f1b3d5aed33c8c3376f4d5ddda",
+				"0x9486083f1abbb2173703602d0f08dff521aa27c44e1d720e5aec53b7f4a73774"
+			];
+		},
+
+		exists: (subName) => {
+			return false;
+		}
 	}
 }
