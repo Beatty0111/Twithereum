b7671f37f0ab86f44a733073b619ffc5e3cb3f2c
Tylor Beatty <tylorbeatty1@gmail.com>
2019-03-03 01:42:24 -0500

    ...

---
+++ src/components/LikeButton.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+class LikeButton extends Component
+{
+    constructor(props){
+        super(props);
+
+        this.state = {
+            value: false,
+        }
+    }
+
+    render(){
+        <div className = "likeButton">
+            
+        </div>
+    }
+}
