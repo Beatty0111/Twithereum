6a04deba20ae63a66a105d328971a6c19553e5c9
Ryan Young <ryanyoung1@hotmail.com>
2019-03-03 10:51:30 -0500

    Fixed PostService Export

--- src/ApiServices/PostService.js
+++ src/ApiServices/PostService.js
@@ -1,7 +1,9 @@
 import Config from './../config.js';
 
+let PostService;
+
 if (Config.mockup == false) {
-	const PostService = {
+	PostService = {
 		getPost: (address) => {
 			
 		},
@@ -9,10 +11,9 @@ if (Config.mockup == false) {
 
 		}
 	}
-	export default PostService;
 } else {
 	// Mock Up
-	const PostService = {
+	PostService = {
 		getPost: (address) => {
 			let data = {
 				data: "This stuff in here is pretty cool. The longer you look at it is just becomes that much cooler. I see you are still reading this. It really shows how awesome this post is right. Yeah.",
@@ -27,5 +28,6 @@ if (Config.mockup == false) {
 			return;
 		}
 	}
-	export default PostService;
 }
+
+export default PostService;
