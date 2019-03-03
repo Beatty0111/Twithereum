8eac5e898f8ced770043973619923deb7da26924
Tylor Beatty <tylorbeatty1@gmail.com>
2019-03-03 09:58:23 -0500

    Added search box, need formating etc.

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,14 +1,39 @@
 import React, { Component } from "react";
 import UserService from '../ApiServices/UserService.js'
 
-class Posts extends Component {
+class Posts extends React.Component {
+  constructor(props) {
+     super(props);
+     
+     this.state = {
+        data: '0x3231B0004588d8685Ee50D06B28E96ba4EEec82D'
+     }
+     this.updateState = this.updateState.bind(this);
+  };
+  updateState(e) {
+     this.setState({data: e.target.value});
+  }
+  render() {
+     return (
+        <div>
+           <input type = "text" value = {this.state.data} 
+              onChange = {this.updateState} />
+           <h4>{this.state.data}</h4>
+
+           <PostsList search={this.state.data}/>
+        </div>
+     );
+  }
+}
+
+class PostsList extends Component {
 
   render() {
     return (
       <div>
         <h2>Posts</h2>
           <ul>
-            {UserService.getPosts().map(function(post,index){
+            {UserService.getPosts(this.props.search).map(function(post,index){
               return (
                 <div className="card tweet-post">
                   <div class="card-body">
