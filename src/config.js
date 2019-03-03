49ce8a2333b91e93919d4945206cac22f89de6d8
Tylor Beatty <tylorbeatty1@gmail.com>
2019-03-03 08:24:51 -0500

    new masternode

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 import Environment from './Enums/Environment.js';
 
 const Config = {
-	masterNodeAddr: "0x0000000000000000000000000000000000000000",
+	masterNodeAddr: "0x674b1c6830655abc5b257fdd70314dbfff31d793",
 	mockup: true,
 	environment: Environment.debug
 };
